7d3759a3c49ee146af0c495f2b20ed1bbae475a1
vchacin <victoria.chacin@vtex.com.br>
2022-08-02 21:53:44 -0300

    Primer homework

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,9 +1,25 @@
 'use strict'
 
 function BinarioADecimal(num) {
+  let suma = 0;
+
+  for (let i = 0; i < num.length; i++) {
+    suma += +num[i] * 2 ** (num.length - 1 - i);
+  }
+
+  return suma;
 }
 
 function DecimalABinario(num) {
+  let binario = [];
+  
+  while (num > 0){
+    let digito = num % 2;
+    num = Math.floor(num / 2);
+    binario.unshift(digito);
+  }
+  
+  return binario.join("");
 }
 
 

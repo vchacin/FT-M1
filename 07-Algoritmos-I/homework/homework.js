7d35b571e7fc50f1a508dbc3abad813b4df51233
vchacin <arq.vchacin@gmail.com>
2022-08-24 01:30:48 -0300

    Primera parte de la homework

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -7,7 +7,32 @@ function factorear(num) {
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
 
+  // function esPrimo(numero) {
+
+  //   if (numero < 2) return false;
+  //   if (numero === 2) return true;
+    
+  //   for(var i = 2; i < numero; i++) {
+
+  //     if(numero % i === 0) {
+  //       return false;
+  //     }
+
+  //   }
+
+  //   return true;
+  // }
+  // g
+  // if (num < 0 || num < 2) return false;
+  // let lista = [];
+  // while(num/2 == 0){
+  //   num = num/2;
+  //   lista.push(num);
+  // }
+  // console.log(lista);
 }
+
+factorear(33);
 
 function bubbleSort(array) {
   // Implementar el método conocido como bubbleSort para ordenar de menor a mayor
@@ -15,6 +40,23 @@ function bubbleSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  console.time('loop');
+  let isSorted = false;
+  while (!isSorted){
+    isSorted = true;
+    let lastSorted = array.length-1;
+    for (let i = 0; i < array.length-1; i++){
+      if (array[i] > array[i+1]){
+        var aux = array[i];
+        array[i] = array[i+1];
+        array[i+1] = aux;
+        isSorted = false;
+        lastSorted--;
+      }
+    }
+  }
+  console.timeEnd('loop');
+  return array;
 }
 
 
@@ -24,8 +66,22 @@ function insertionSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
-}
+  //[7,4,5,2]
+  //   i
+  // j
 
+
+  for (let i = 1; i<array.length; i++){
+    var j = i-1;
+    var aux = array[i];
+    while (j >= 0 && array[j] > aux){
+      array[j+1] = array[j];
+      j--;
+    }
+    array[j+1] = aux;
+  }
+  return array;
+}
 
 function selectionSort(array) {
   // Implementar el método conocido como selectionSort para ordenar de menor a mayor

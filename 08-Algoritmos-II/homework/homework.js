4b32d786611a6138a6d2bb172a41c05724bf07b4
vchacin <arq.vchacin@gmail.com>
2022-08-27 18:41:52 -0300

    Homework in progress

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,6 +6,31 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+   // [1,2,4,23,5,88,6]
+  
+  //          [6]
+  // [1,2,4,5]  [23,88]
+  
+  // [1,2,4]
+  // [23]
+  
+  if (array.length <= 1) return array;
+  
+  let pivote = array[array.length-1];
+  let menorArray = [];
+  let mayorArray = [];
+  
+  // Comienzo a iterar despues o antes del pivote que defini
+  for (let i = 0; i < array.length-1; i++){
+    if (array[i] < pivote){
+      menorArray.push(array[i]);
+    } else {
+      mayorArray.push(array[i]);
+    }
+  }
+ 
+  // recursion
+  return quickSort(menorArray).concat(pivote).concat(quickSort(mayorArray));
 
 }
 
